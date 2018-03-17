6ddd226ffbae9c84a01c02cbf6fe574ed751786c
thomasdunlap <tom.a.dunlap@gmail.com>
2018-03-17 19:48:49 -0400

    fix: WORKING MODEL. RMSE values for VX VY are over 2,000, so thinking I may have to refactor that calculation.

--- src/tools.cpp
+++ src/tools.cpp
@@ -19,7 +19,7 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
 
   // Sum of squared residuals
   for (int i=0; i < estimations.size(); ++i) {
-    VectorXd residuals = estimations.size() - ground_truth[i];
+    VectorXd residuals = estimations[i] - ground_truth[i];
     residuals = residuals.array() * residuals.array();
     rmse += residuals;
   }
