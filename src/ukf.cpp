ceb184269da1381889da03f07233fe09756bbdfa
thomasdunlap <tom.a.dunlap@gmail.com>
2018-03-21 20:18:33 -0400

    fix: refactored tuning parameters.

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -27,7 +27,7 @@ UKF::UKF() {
   std_a_ = 0.6;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 0.6;
+  std_yawdd_ = 0.55;
 
   // Laser measurement noise standard deviation position1 in m
   std_laspx_ = 0.15;
@@ -45,20 +45,19 @@ UKF::UKF() {
   std_radrd_ = 0.3;
 
   /**
-  TODO:
   Complete the initialization. See ukf.h for other member properties.
   Hint: one or more values initialized above might be wildly off...
   */
   n_x_ = 5;
   n_aug_ = 7;
-  int n_sig = 2 * n_aug_ + 1;
+  //int n_sig = 2 * n_aug_ + 1;
   lambda_ = 3 - n_aug_;
-  Xsig_pred_ = MatrixXd(n_x_, n_sig);
+  Xsig_pred_ = MatrixXd(n_x_, 2 * n_aug_ + 1);
   // Set weights
-  weights_ = VectorXd(n_sig);
+  weights_ = VectorXd(2 * n_aug_ + 1);
   weights_(0) = (double)lambda_ / (lambda_ + n_aug_);
   double weights_val = 1.0 / (2.0 * (lambda_ + n_aug_));
-  weights_.bottomRows(n_sig-1).fill(weights_val);
+  weights_.bottomRows(2 * n_aug_).fill(weights_val);
 }
 
 UKF::~UKF() {}
@@ -69,7 +68,6 @@ UKF::~UKF() {}
  */
 void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
   /**
-  TODO:
   Complete this function! Make sure you switch between lidar and radar
   measurements.
   */
@@ -82,9 +80,9 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
     // Covariance matrix
     P_ << 1.0, 0.0, 0.0, 0.0, 0.0,
           0.0, 1.0, 0.0, 0.0, 0.0,
-          0.0, 0.0, 20.0, 0.0, 0.0,
-          0.0, 0.0, 0.0, 13.15, 0.0,
-          0.0, 0.0, 0.0, 0.0, 0.1;
+          0.0, 0.0, 1.0, 0.0, 0.0,
+          0.0, 0.0, 0.0, 1.0, 0.0,
+          0.0, 0.0, 0.0, 0.0, 1.0;
 
     if (meas_package.sensor_type_ == MeasurementPackage::RADAR) {
       float rho = meas_package.raw_measurements_(0);
@@ -93,7 +91,7 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
       x_(0) = rho * cos(phi);
       x_(1) = rho * sin(phi);
       x_(2) = rho_dot;
-      P_(2,2) = 1;
+
     }
     else if (meas_package.sensor_type_ == MeasurementPackage::LASER) {
       x_(0) = meas_package.raw_measurements_(0);
@@ -133,7 +131,6 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
  */
 void UKF::Prediction(double delta_t) {
   /**
-  TODO:
   Complete this function! Estimate the object's location. Modify the state
   vector, x_. Predict sigma points, the state, and the state covariance matrix.
   */
@@ -178,13 +175,13 @@ void UKF::Prediction(double delta_t) {
   //predict sigma points
   for (int i = 0; i< n_sig; i++) {
     //extract values for better readability
-    double p_x = Xsig_aug(0,i);
-    double p_y = Xsig_aug(1,i);
-    double v = Xsig_aug(2,i);
-    double yaw = Xsig_aug(3,i);
-    double yawd = Xsig_aug(4,i);
-    double nu_a = Xsig_aug(5,i);
-    double nu_yawdd = Xsig_aug(6,i);
+    double p_x = Xsig_aug(0, i);
+    double p_y = Xsig_aug(1, i);
+    double v = Xsig_aug(2, i);
+    double yaw = Xsig_aug(3, i);
+    double yawd = Xsig_aug(4, i);
+    double nu_a = Xsig_aug(5, i);
+    double nu_yawdd = Xsig_aug(6, i);
 
     double delta_t2 = delta_t * delta_t;
     double yawd_dt = yawd * delta_t;
